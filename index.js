7b5bdb9f4ab235830c48e4a33bf4f2b9a682ba64
John Ku <john.cmyk@gmail.com>
2019-02-03 14:50:13 -0800

    template

---
+++ index.js
@@ -0,0 +1,41 @@
+let renderer
+let camera
+let scene
+
+let width = window.innerWidth
+let height = window.innerHeight
+
+let model
+
+let mixer
+let clock = new THREE.Clock()
+
+const init = () => {
+  camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000)
+  scene = new THREE.Scene()
+  camera.position.z = 100
+
+  renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setSize(width, height)
+  document.body.appendChild(renderer.domElement)
+
+  const loader = new THREE.GLTFLoader()
+  loader.load(
+    'https://uploads.codesandbox.io/uploads/user/cdf06b01-5469-4a87-a256-1cc15b068232/1F9I-test.gltf',
+    g => {
+      const torus = g.scene.children[2]
+      console.log(torus)
+      scene.add(torus)
+      model = torus
+    }
+  )
+  renderer.render(scene, camera)
+}
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
+
+init()
+// animate()
