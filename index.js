e72863f17e66237ebeb530fc39a52611d03b8d14
John Ku <john.cmyk@gmail.com>
2019-02-03 16:17:41 -0800

    i

--- index.js
+++ index.js
@@ -8,33 +8,44 @@ let scene
 let width = window.innerWidth - 40
 let height = window.innerHeight - 40
 
+let model
+
 const app = document.getElementById('app')
 
 const init = () => {
-  camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000)
+  camera = new THREE.PerspectiveCamera(45, width / height, 0.25, 30)
   scene = new THREE.Scene()
-  camera.position.z = 100
+  camera.position.set(0, 0, 2.7)
 
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(width, height)
 
-  app.innerHTML = ''
-  app.appendChild(renderer.domElement)
+  const light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
+  light.position.set( 0, 1, 0 );
+  scene.add( light );
 
   const loader = new THREE.GLTFLoader()
   loader.load(
     './test.glb',
     gltf => {
-      scene.add(gltf.scene)
+      const s = gltf.scene
+      scene.add(s)
+      model = s.children[2]
     }
   )
+
+  app.innerHTML = ''
+  app.appendChild(renderer.domElement)
   renderer.render(scene, camera)
 }
 
 const animate = () => {
   requestAnimationFrame(animate)
+  if (model) {
+    model.rotation.x += 0.002
+  }
   renderer.render(scene, camera)
 }
 
 init()
-// animate()
+animate()
